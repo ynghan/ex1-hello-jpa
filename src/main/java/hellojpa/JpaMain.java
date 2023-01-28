7166d7311c08f7bda46f0c33ce78da7185ff2520
ynghan <github.com/ynghan>
2023-01-28 20:00:06 +0900

    start jpa basic

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,27 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try{
+            //영속
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+            em.flush();
+
+            System.out.println("==========");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
